a921102190ea88d69e920a1d6c3559dd1647725d
Gabriel Luezas <130647529+GabrielLuezas@users.noreply.github.com>
2024-10-25 13:25:03 +0200

    Diseño login actualizado

--- Formularios/Formularios/Login.Designer.cs
+++ Formularios/Formularios/Login.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace Formularios.Formularios
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Formularios.Formularios
 {
     partial class Login
     {
@@ -28,79 +32,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LBLEmail = new System.Windows.Forms.Label();
-            this.TXTBEmail = new System.Windows.Forms.TextBox();
-            this.LBLPass = new System.Windows.Forms.Label();
-            this.TXTBPass = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panelDerecho = new System.Windows.Forms.Panel();
             this.BTNLogin = new System.Windows.Forms.Button();
+            this.TXTBPass = new System.Windows.Forms.TextBox();
+            this.LBLPass = new System.Windows.Forms.Label();
+            this.TXTBEmail = new System.Windows.Forms.TextBox();
+            this.LBLEmail = new System.Windows.Forms.Label();
+            this.LBLTitulo = new System.Windows.Forms.Label();
+            this.panelIzquierdo = new System.Windows.Forms.Panel();
+            this.panelDerecho.SuspendLayout();
             this.SuspendLayout();
             // 
-            // LBLEmail
+            // panelDerecho
             // 
-            this.LBLEmail.AutoSize = true;
-            this.LBLEmail.Location = new System.Drawing.Point(352, 74);
-            this.LBLEmail.Name = "LBLEmail";
-            this.LBLEmail.Size = new System.Drawing.Size(93, 13);
-            this.LBLEmail.TabIndex = 0;
-            this.LBLEmail.Text = "Correo electronico";
+            this.panelDerecho.BackColor = System.Drawing.Color.White;
+            this.panelDerecho.Controls.Add(this.BTNLogin);
+            this.panelDerecho.Controls.Add(this.TXTBPass);
+            this.panelDerecho.Controls.Add(this.LBLPass);
+            this.panelDerecho.Controls.Add(this.TXTBEmail);
+            this.panelDerecho.Controls.Add(this.LBLEmail);
+            this.panelDerecho.Controls.Add(this.LBLTitulo);
+            this.panelDerecho.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelDerecho.Location = new System.Drawing.Point(333, 0);
+            this.panelDerecho.Name = "panelDerecho";
+            this.panelDerecho.Size = new System.Drawing.Size(351, 311);
+            this.panelDerecho.TabIndex = 0;
             // 
-            // TXTBEmail
+            // BTNLogin
             // 
-            this.TXTBEmail.Location = new System.Drawing.Point(348, 100);
-            this.TXTBEmail.Name = "TXTBEmail";
-            this.TXTBEmail.Size = new System.Drawing.Size(100, 20);
-            this.TXTBEmail.TabIndex = 1;
+            this.BTNLogin.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.BTNLogin.Location = new System.Drawing.Point(0, 0);
+            this.BTNLogin.Name = "BTNLogin";
+            this.BTNLogin.Size = new System.Drawing.Size(75, 23);
+            this.BTNLogin.TabIndex = 0;
+            // 
+            // TXTBPass
+            // 
+            this.TXTBPass.Location = new System.Drawing.Point(65, 180);
+            this.TXTBPass.Name = "TXTBPass";
+            this.TXTBPass.PasswordChar = '*';
+            this.TXTBPass.Size = new System.Drawing.Size(210, 20);
+            this.TXTBPass.TabIndex = 4;
             // 
             // LBLPass
             // 
             this.LBLPass.AutoSize = true;
-            this.LBLPass.Location = new System.Drawing.Point(366, 138);
+            this.LBLPass.Location = new System.Drawing.Point(65, 160);
             this.LBLPass.Name = "LBLPass";
             this.LBLPass.Size = new System.Drawing.Size(61, 13);
-            this.LBLPass.TabIndex = 2;
+            this.LBLPass.TabIndex = 3;
             this.LBLPass.Text = "Contraseña";
             // 
-            // TXTBPass
+            // TXTBEmail
             // 
-            this.TXTBPass.Location = new System.Drawing.Point(347, 163);
-            this.TXTBPass.Name = "TXTBPass";
-            this.TXTBPass.PasswordChar = '*';
-            this.TXTBPass.Size = new System.Drawing.Size(100, 20);
-            this.TXTBPass.TabIndex = 3;
+            this.TXTBEmail.Location = new System.Drawing.Point(65, 120);
+            this.TXTBEmail.Name = "TXTBEmail";
+            this.TXTBEmail.Size = new System.Drawing.Size(210, 20);
+            this.TXTBEmail.TabIndex = 2;
             // 
-            // BTNLogin
+            // LBLEmail
             // 
-            this.BTNLogin.Location = new System.Drawing.Point(358, 208);
-            this.BTNLogin.Name = "BTNLogin";
-            this.BTNLogin.Size = new System.Drawing.Size(75, 23);
-            this.BTNLogin.TabIndex = 4;
-            this.BTNLogin.Text = "Login";
-            this.BTNLogin.UseVisualStyleBackColor = true;
-            this.BTNLogin.Click += new System.EventHandler(this.BTNLogin_Click);
+            this.LBLEmail.AutoSize = true;
+            this.LBLEmail.Location = new System.Drawing.Point(65, 100);
+            this.LBLEmail.Name = "LBLEmail";
+            this.LBLEmail.Size = new System.Drawing.Size(91, 13);
+            this.LBLEmail.TabIndex = 1;
+            this.LBLEmail.Text = "CorreoElectronico";
+            // 
+            // LBLTitulo
+            // 
+            this.LBLTitulo.AutoSize = true;
+            this.LBLTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.LBLTitulo.Location = new System.Drawing.Point(65, 50);
+            this.LBLTitulo.Name = "LBLTitulo";
+            this.LBLTitulo.Size = new System.Drawing.Size(70, 30);
+            this.LBLTitulo.TabIndex = 0;
+            this.LBLTitulo.Text = "Login";
+            // 
+            // panelIzquierdo
+            // 
+            this.panelIzquierdo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelIzquierdo.BackgroundImage")));
+            this.panelIzquierdo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panelIzquierdo.Name = "panelIzquierdo";
+            this.panelIzquierdo.Size = new System.Drawing.Size(334, 311);
+            this.panelIzquierdo.TabIndex = 1;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.BTNLogin);
-            this.Controls.Add(this.TXTBPass);
-            this.Controls.Add(this.LBLPass);
-            this.Controls.Add(this.TXTBEmail);
-            this.Controls.Add(this.LBLEmail);
+            this.ClientSize = new System.Drawing.Size(684, 311);
+            this.Controls.Add(this.panelDerecho);
+            this.Controls.Add(this.panelIzquierdo);
             this.Name = "Login";
             this.Text = "Login";
+            this.panelDerecho.ResumeLayout(false);
+            this.panelDerecho.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label LBLEmail;
-        private System.Windows.Forms.TextBox TXTBEmail;
-        private System.Windows.Forms.Label LBLPass;
-        private System.Windows.Forms.TextBox TXTBPass;
-        private System.Windows.Forms.Button BTNLogin;
+        private System.Windows.Forms.Panel panelIzquierdo;
+        private Panel panelDerecho;
+        private Label LBLTitulo;
+        private Button BTNLogin;
+        private TextBox TXTBPass;
+        private Label LBLPass;
+        private TextBox TXTBEmail;
+        private Label LBLEmail;
     }
 }
--- Formularios/Formularios/Login.cs
+++ Formularios/Formularios/Login.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace Formularios.Formularios
         public Login()
         {
             InitializeComponent();
+            PersonalizarBotonLogin();
         }
 
         private void BTNLogin_Click(object sender, EventArgs e)
@@ -31,6 +33,46 @@ namespace Formularios.Formularios
             {
                 MessageBox.Show("Los datos introducidos no son validos");
             }
+        }
+        private void PersonalizarBotonLogin()
+        {
+            // Configuraciones del botón
+            this.BTNLogin.Enabled = false;
+            this.BTNLogin.Text = "Login";
+            this.BTNLogin.Size = new Size(120, 35);
+            this.BTNLogin.Location = new Point(120, 220);
+            this.BTNLogin.BackColor = Color.RoyalBlue;
+            this.BTNLogin.ForeColor = Color.White;
+            this.BTNLogin.FlatStyle = FlatStyle.Flat;
+            this.BTNLogin.FlatAppearance.BorderSize = 0;
+
+            // Redondear bordes del botón
+            this.BTNLogin.Paint += (sender, e) =>
+            {
+                Graphics g = e.Graphics;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                GraphicsPath path = new GraphicsPath();
+                path.AddArc(0, 0, 20, 20, 180, 90);
+                path.AddArc(this.BTNLogin.Width - 20, 0, 20, 20, 270, 90);
+                path.AddArc(this.BTNLogin.Width - 20, this.BTNLogin.Height - 20, 20, 20, 0, 90);
+                path.AddArc(0, this.BTNLogin.Height - 20, 20, 20, 90, 90);
+                path.CloseFigure();
+                this.BTNLogin.ForeColor = Color.White;
+                this.BTNLogin.Region = new Region(path);
+            };
+
+            // Habilitar el botón solo si los campos están llenos
+            TXTBEmail.TextChanged += (sender, e) =>
+            {
+                this.BTNLogin.Enabled = !string.IsNullOrWhiteSpace(TXTBEmail.Text) && !string.IsNullOrWhiteSpace(TXTBPass.Text);
+            };
+            TXTBPass.TextChanged += (sender, e) =>
+            {
+                this.BTNLogin.Enabled = !string.IsNullOrWhiteSpace(TXTBEmail.Text) && !string.IsNullOrWhiteSpace(TXTBPass.Text);
+            };
+
+            // Evento Click del botón
+            this.BTNLogin.Click += new System.EventHandler(this.BTNLogin_Click);
         }
     }
 }
