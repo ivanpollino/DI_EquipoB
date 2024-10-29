b932226312f421fc46190c26c6eb59d21cc47189
Aleaand <alejandraandrade1224@gmail.com>
2024-10-29 19:55:52 +0100

    Progreso de landing

--- Formularios/Formularios/Landing.Designer.cs
+++ Formularios/Formularios/Landing.Designer.cs
@@ -34,6 +34,8 @@
             this.btLogin = new System.Windows.Forms.Button();
             this.btRegistro = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lbTexto1 = new System.Windows.Forms.Label();
+            this.lbTexto2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,19 +55,19 @@
             // 
             this.lbLinkLogin.AutoSize = true;
             this.lbLinkLogin.BackColor = System.Drawing.Color.Transparent;
-            this.lbLinkLogin.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbLinkLogin.LinkColor = System.Drawing.Color.Teal;
-            this.lbLinkLogin.Location = new System.Drawing.Point(354, 16);
+            this.lbLinkLogin.Font = new System.Drawing.Font("Segoe UI Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLinkLogin.LinkColor = System.Drawing.Color.SlateGray;
+            this.lbLinkLogin.Location = new System.Drawing.Point(387, 17);
             this.lbLinkLogin.Name = "lbLinkLogin";
-            this.lbLinkLogin.Size = new System.Drawing.Size(127, 20);
+            this.lbLinkLogin.Size = new System.Drawing.Size(50, 20);
             this.lbLinkLogin.TabIndex = 1;
             this.lbLinkLogin.TabStop = true;
-            this.lbLinkLogin.Text = "Login de Usuario";
+            this.lbLinkLogin.Text = "LOGIN";
             this.lbLinkLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lbLinkLogin_LinkClicked);
             // 
             // btLogin
             // 
-            this.btLogin.Location = new System.Drawing.Point(0, 275);
+            this.btLogin.Location = new System.Drawing.Point(358, 329);
             this.btLogin.Name = "btLogin";
             this.btLogin.Size = new System.Drawing.Size(121, 23);
             this.btLogin.TabIndex = 2;
@@ -75,7 +77,7 @@
             // 
             // btRegistro
             // 
-            this.btRegistro.Location = new System.Drawing.Point(0, 304);
+            this.btRegistro.Location = new System.Drawing.Point(358, 358);
             this.btRegistro.Name = "btRegistro";
             this.btRegistro.Size = new System.Drawing.Size(121, 23);
             this.btRegistro.TabIndex = 3;
@@ -87,6 +89,8 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel1.BackgroundImage = global::Formularios.Properties.Resources.img_gym;
+            this.panel1.Controls.Add(this.lbTexto2);
+            this.panel1.Controls.Add(this.lbTexto1);
             this.panel1.Controls.Add(this.lbLinkLogin);
             this.panel1.Controls.Add(this.btRegistro);
             this.panel1.Controls.Add(this.lbInfoUsuario);
@@ -96,6 +100,31 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(884, 461);
             this.panel1.TabIndex = 4;
+            // 
+            // lbTexto1
+            // 
+            this.lbTexto1.AutoSize = true;
+            this.lbTexto1.BackColor = System.Drawing.Color.Transparent;
+            this.lbTexto1.Font = new System.Drawing.Font("Segoe UI Black", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTexto1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lbTexto1.Location = new System.Drawing.Point(110, 102);
+            this.lbTexto1.Name = "lbTexto1";
+            this.lbTexto1.Size = new System.Drawing.Size(641, 86);
+            this.lbTexto1.TabIndex = 4;
+            this.lbTexto1.Text = "TU MÁXIMO NIVEL";
+            this.lbTexto1.Click += new System.EventHandler(this.lbTexto1_Click);
+            // 
+            // lbTexto2
+            // 
+            this.lbTexto2.AutoSize = true;
+            this.lbTexto2.BackColor = System.Drawing.Color.Transparent;
+            this.lbTexto2.Font = new System.Drawing.Font("Segoe UI Black", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lbTexto2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lbTexto2.Location = new System.Drawing.Point(234, 188);
+            this.lbTexto2.Name = "lbTexto2";
+            this.lbTexto2.Size = new System.Drawing.Size(393, 86);
+            this.lbTexto2.TabIndex = 5;
+            this.lbTexto2.Text = "ES POSIBLE";
             // 
             // Landing
             // 
@@ -120,5 +149,7 @@
         private System.Windows.Forms.Button btLogin;
         private System.Windows.Forms.Button btRegistro;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbTexto1;
+        private System.Windows.Forms.Label lbTexto2;
     }
 }
