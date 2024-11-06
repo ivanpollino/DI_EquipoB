53d2a5f84e4ad792a00777eafa74929bdb708356
Aleaand <alejandraandrade1224@gmail.com>
2024-11-06 17:45:35 +0100

    Base de Form Registro de Actividad

---
+++ Formularios/Formularios/RegistrarActividad.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Formularios.Formularios
+{
+    partial class RegistrarActividad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LBLTitulo = new System.Windows.Forms.Label();
+            this.lbNombreActv = new System.Windows.Forms.Label();
+            this.lbDescripcion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxDescripcionAct = new System.Windows.Forms.TextBox();
+            this.tbxNombreAct = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btRegistrarAct = new System.Windows.Forms.Button();
+            this.btLimpiarForm = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.btLimpiarForm);
+            this.panel1.Controls.Add(this.btRegistrarAct);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.tbxNombreAct);
+            this.panel1.Controls.Add(this.tbxDescripcionAct);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbDescripcion);
+            this.panel1.Controls.Add(this.lbNombreActv);
+            this.panel1.Controls.Add(this.LBLTitulo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // LBLTitulo
+            // 
+            this.LBLTitulo.AutoSize = true;
+            this.LBLTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.LBLTitulo.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Bold);
+            this.LBLTitulo.ForeColor = System.Drawing.Color.DarkCyan;
+            this.LBLTitulo.Location = new System.Drawing.Point(58, 31);
+            this.LBLTitulo.Name = "LBLTitulo";
+            this.LBLTitulo.Size = new System.Drawing.Size(408, 51);
+            this.LBLTitulo.TabIndex = 28;
+            this.LBLTitulo.Text = "Registro de Actividad";
+            // 
+            // lbNombreActv
+            // 
+            this.lbNombreActv.AutoSize = true;
+            this.lbNombreActv.BackColor = System.Drawing.Color.Transparent;
+            this.lbNombreActv.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lbNombreActv.ForeColor = System.Drawing.Color.White;
+            this.lbNombreActv.Location = new System.Drawing.Point(63, 103);
+            this.lbNombreActv.Name = "lbNombreActv";
+            this.lbNombreActv.Size = new System.Drawing.Size(168, 19);
+            this.lbNombreActv.TabIndex = 29;
+            this.lbNombreActv.Text = "Nombre de la actividad";
+            // 
+            // lbDescripcion
+            // 
+            this.lbDescripcion.AutoSize = true;
+            this.lbDescripcion.BackColor = System.Drawing.Color.Transparent;
+            this.lbDescripcion.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lbDescripcion.ForeColor = System.Drawing.Color.White;
+            this.lbDescripcion.Location = new System.Drawing.Point(63, 155);
+            this.lbDescripcion.Name = "lbDescripcion";
+            this.lbDescripcion.Size = new System.Drawing.Size(190, 19);
+            this.lbDescripcion.TabIndex = 30;
+            this.lbDescripcion.Text = "Descripción de la actividad";
+            // 
+            // label1
+            // 
+            this.label1.AccessibleDescription = "lbMonitor";
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(63, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 19);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Monitor asociado";
+            // 
+            // tbxDescripcionAct
+            // 
+            this.tbxDescripcionAct.Location = new System.Drawing.Point(67, 177);
+            this.tbxDescripcionAct.Name = "tbxDescripcionAct";
+            this.tbxDescripcionAct.Size = new System.Drawing.Size(284, 20);
+            this.tbxDescripcionAct.TabIndex = 33;
+            // 
+            // tbxNombreAct
+            // 
+            this.tbxNombreAct.Location = new System.Drawing.Point(67, 125);
+            this.tbxNombreAct.Name = "tbxNombreAct";
+            this.tbxNombreAct.Size = new System.Drawing.Size(284, 20);
+            this.tbxNombreAct.TabIndex = 34;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(67, 227);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 56);
+            this.listBox1.TabIndex = 35;
+            // 
+            // btRegistrarAct
+            // 
+            this.btRegistrarAct.Location = new System.Drawing.Point(67, 314);
+            this.btRegistrarAct.Name = "btRegistrarAct";
+            this.btRegistrarAct.Size = new System.Drawing.Size(75, 23);
+            this.btRegistrarAct.TabIndex = 36;
+            this.btRegistrarAct.Text = " Registrar Actividad";
+            this.btRegistrarAct.UseVisualStyleBackColor = true;
+            // 
+            // btLimpiarForm
+            // 
+            this.btLimpiarForm.Location = new System.Drawing.Point(148, 314);
+            this.btLimpiarForm.Name = "btLimpiarForm";
+            this.btLimpiarForm.Size = new System.Drawing.Size(75, 23);
+            this.btLimpiarForm.TabIndex = 37;
+            this.btLimpiarForm.Text = "Limpiar Form";
+            this.btLimpiarForm.UseVisualStyleBackColor = true;
+            // 
+            // RegistrarActividad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "RegistrarActividad";
+            this.Text = "RegistrarActividad";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LBLTitulo;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox tbxNombreAct;
+        private System.Windows.Forms.TextBox tbxDescripcionAct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbDescripcion;
+        private System.Windows.Forms.Label lbNombreActv;
+        private System.Windows.Forms.Button btLimpiarForm;
+        private System.Windows.Forms.Button btRegistrarAct;
+    }
+}
