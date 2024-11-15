919e529347694a5f26efc310145bd327af92d41e
Gabriel Luezas <130647529+GabrielLuezas@users.noreply.github.com>
2024-11-15 11:27:30 +0100

    ControlPersonalizado

---
+++ Presentacion/ComponentesPersonalizados/ActividadPestaña.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio.EntitiesDTO;
+
+namespace Presentacion.ComponentesPersonalizados
+{
+    public partial class ActividadPestaña : UserControl
+    {
+        public ActividadPestaña()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ActividadDTO actividadDTO = new ActividadDTO();
+            actividadDTO.Nombre = LBLDondeVaNombreActividad.Text;
+            new Negocio.Managment.ActividadManagment().bajaActividad(actividadDTO);
+        }
+    }
+}
--- Presentacion/Formularios/ListadoActividades.Designer.cs
+++ Presentacion/Formularios/ListadoActividades.Designer.cs
@@ -29,64 +29,26 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.userControl11 = new Presentacion.ComponentesPersonalizados.UserControl1();
-            this.userControl12 = new Presentacion.ComponentesPersonalizados.UserControl1();
-            this.userControl13 = new Presentacion.ComponentesPersonalizados.UserControl1();
-            this.userControl14 = new Presentacion.ComponentesPersonalizados.UserControl1();
-            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Controls.Add(this.userControl11);
-            this.flowLayoutPanel1.Controls.Add(this.userControl12);
-            this.flowLayoutPanel1.Controls.Add(this.userControl13);
-            this.flowLayoutPanel1.Controls.Add(this.userControl14);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, -2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(881, 575);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(897, 610);
             this.flowLayoutPanel1.TabIndex = 0;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
-            // 
-            // userControl11
-            // 
-            this.userControl11.Location = new System.Drawing.Point(3, 3);
-            this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(150, 150);
-            this.userControl11.TabIndex = 0;
-            // 
-            // userControl12
-            // 
-            this.userControl12.Location = new System.Drawing.Point(159, 3);
-            this.userControl12.Name = "userControl12";
-            this.userControl12.Size = new System.Drawing.Size(150, 150);
-            this.userControl12.TabIndex = 1;
-            // 
-            // userControl13
-            // 
-            this.userControl13.Location = new System.Drawing.Point(315, 3);
-            this.userControl13.Name = "userControl13";
-            this.userControl13.Size = new System.Drawing.Size(150, 150);
-            this.userControl13.TabIndex = 2;
-            // 
-            // userControl14
-            // 
-            this.userControl14.Location = new System.Drawing.Point(471, 3);
-            this.userControl14.Name = "userControl14";
-            this.userControl14.Size = new System.Drawing.Size(150, 150);
-            this.userControl14.TabIndex = 3;
             // 
             // ListadoActividades
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(881, 571);
+            this.ClientSize = new System.Drawing.Size(897, 610);
             this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ListadoActividades";
             this.Text = "ListadoActividades";
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -94,9 +56,5 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private ComponentesPersonalizados.UserControl1 userControl11;
-        private ComponentesPersonalizados.UserControl1 userControl12;
-        private ComponentesPersonalizados.UserControl1 userControl13;
-        private ComponentesPersonalizados.UserControl1 userControl14;
     }
 }
